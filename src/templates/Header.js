9df549a3c47262b4241052c2d77d72e0bb625d32
Agustin Moran <agustin.moranr@gmail.com>
2020-10-25 21:14:38 -0600

    [Fix] Header bug

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">100tifico</a>
+          <a href="/rick-and-morty-SPA">100tifico</a>
         </h1>
       </div>
       <div class="Header-nav">
